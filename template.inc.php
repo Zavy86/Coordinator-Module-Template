a91ae76e0c3d894755a5044a46f85ac09b4ca9e9
Manuel Zavatta <manuel.zavatta@cogne.com>
2014-02-12 11:23:34 +0100

    Template fix

--- template.inc.php
+++ template.inc.php
@@ -2,13 +2,13 @@
 /* -------------------------------------------------------------------------- *\
 |* -[ Module-Template - Template ]------------------------------------------- *|
 \* -------------------------------------------------------------------------- */
-$module="module-template";
+include("module.inc.php");
 // include core api functions
 include("../core/api.inc.php");
 // if exist include module api functions
 if(file_exists("api.inc.php")){include("api.inc.php");}
 // print header
-$html->header(api_text("module-template-title"),$module);
+$html->header(api_text("module-template-title"),$module_name);
 // build navigation tab
 $nt_array=array();
 $nt_array[]=api_navigationTab(api_text("module-template-list"),"module-template_list.php");
@@ -16,7 +16,7 @@ $nt_array[]=api_navigationTab(api_text("module-template-edit"),"module-template_
 // print navigation tab
 api_navigation($nt_array);
 // check permissions before displaying module
-if($checkPermission==NULL){content();}else{if(api_checkPermission($module,$checkPermission,TRUE)){content();}}
+if($checkPermission==NULL){content();}else{if(api_checkPermission($module_name,$checkPermission,TRUE)){content();}}
 // print footer
 $html->footer();
 ?>
