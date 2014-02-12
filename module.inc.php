06169f59d9d693d8bcce48785b0eb373814e1480
Manuel Zavatta <manuel.zavatta@cogne.com>
2014-02-12 11:29:24 +0100

    Hotfix

--- module.inc.php
+++ module.inc.php
@@ -3,7 +3,7 @@
 |* -[ Module-Template - Module Information ]--------------------------------- *|
 \* -------------------------------------------------------------------------- */
 $module_name="module-template";
-$module_version="1.0.0";
+$module_version="1.0.1";
 $module_title="Module Template";
 $module_description="Coordinator Module Template";
 ?>
