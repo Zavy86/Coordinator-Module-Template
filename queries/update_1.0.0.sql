10829412d907ccce960d3f5a14db343a5b54657f
Manuel Zavatta <manuel.zavatta@cogne.com>
2014-02-14 15:40:48 +0100

    Queries fix

--- queries/update_1.0.0.sql
+++ queries/update_1.0.0.sql
@@ -4,14 +4,12 @@
 -- From 1.0.0 to 1.0.1
 --
 
-UPDATE  `settings_modules` SET `version`='1.0.1' WHERE `module`='module-template';
+-- --------------------------------------------------------
+
+UPDATE `settings_modules` SET `version`='1.0.1' WHERE `module`='module-template';
 
 -- --------------------------------------------------------
 
---
--- Update database
---
-
-
+ALTER TABLE `test-module-template` ADD `name` VARCHAR(255) NOT NULL;
 
 -- --------------------------------------------------------
