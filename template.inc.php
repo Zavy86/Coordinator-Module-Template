ed36f18a7bd163bf6c4c66b900d98b7514197379
Manuel Zavatta <manuel.zavatta@cogne.com>
2014-02-28 08:14:39 +0100

    Fix

--- template.inc.php
+++ template.inc.php
@@ -8,11 +8,11 @@ include("../core/api.inc.php");
 // if exist include module api functions
 if(file_exists("api.inc.php")){include("api.inc.php");}
 // print header
-$html->header(api_text("module-template-title"),$module_name);
+$html->header(api_text("module-title"),$module_name);
 // build navigation tab
 $nt_array=array();
-$nt_array[]=api_navigationTab(api_text("module-template-list"),"module-template_list.php");
-$nt_array[]=api_navigationTab(api_text("module-template-edit"),"module-template_edit.php");
+$nt_array[]=api_navigationTab(api_text("nav-list"),"module-template_list.php");
+$nt_array[]=api_navigationTab(api_text("nav-edit"),"module-template_edit.php");
 // print navigation tab
 api_navigation($nt_array);
 // check permissions before displaying module
