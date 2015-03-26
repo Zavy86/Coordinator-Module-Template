c75beba7be9b6dc54125266c6e7fccac2c35c59e
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-03-26 11:51:31 +0100

    Query fix

--- queries/uninstall.sql
+++ queries/uninstall.sql
@@ -4,6 +4,8 @@
 
 -- --------------------------------------------------------
 
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE `test-module-template`;
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- --------------------------------------------------------
