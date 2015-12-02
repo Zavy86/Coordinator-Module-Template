4b8638c20408f58d3529364c26af41c0e1b3fdc2
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-12-02 16:03:48 +0100

    Addresses list

--- module-template_list.php
+++ module-template_list.php
@@ -2,9 +2,43 @@
 /* -------------------------------------------------------------------------- *\
 |* -[ Module-Template - List ]----------------------------------------------- *|
 \* -------------------------------------------------------------------------- */
-$checkPermission="module-template_list";
+// script permission
+$checkPermission="module-template_view";
+// include module template
 include("template.inc.php");
+// script content
 function content(){
- // module-template_list
+ // acquire variables
+ $g_search=$_GET['q'];
+ // show filters
+ echo $GLOBALS['navigation']->filtersText();
+ // build table
+ $addresses_table=new str_table(api_text("module-template_list-tr-unvalued"),TRUE,$GLOBALS['navigation']->filtersGet());
+ $addresses_table->addHeader("&nbsp;",NULL,"16");
+ $addresses_table->addHeader(api_text("module-template_list-th-lastname"),"nowarp",NULL,"`module-template_addresses`.`lastname`");
+ $addresses_table->addHeader(api_text("module-template_list-th-firstname"),"nowarp",NULL,"`module-template_addresses`.`firstname`");
+ $addresses_table->addHeader(api_text("module-template_list-th-address"),NULL,"100%");
+ // get addresses
+ $addresses=api_moduleTemplate_addresses($g_search,TRUE);
+ foreach($addresses->results as $address){
+  // check selected
+  if($address->id==$_GET['idAddress']){$tr_class="info";}else{$tr_class=NULL;}
+  // build address table row
+  $addresses_table->addRow($tr_class);
+  // build table fields
+  $addresses_table->addField(api_link("module-template_view.php?idAddress=".$address->id,api_icon("icon-search",api_text("module-template_list-td-view"))),"nowarp");
+  $addresses_table->addField($address->lastname,"nowarp");
+  $addresses_table->addField($address->firstname,"nowarp");
+  $addresses_table->addField($address->address);
+ }
+ // show table
+ $addresses_table->render();
+ // renderize the pagination
+ $addresses->pagination->render();
+ // debug
+ if($_SESSION["account"]->debug){
+  pre_var_dump($addresses->query,"print","query");
+  pre_var_dump($addresses->results,"print","addresses");
+ }
 }
 ?>
